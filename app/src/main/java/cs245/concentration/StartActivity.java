50cd300942e05d0435f63346d1c2c09b7107697d
Seungyun Lee <sylee714@gmail.com>
2017-02-22 23:50:24 -0800

    Added StartActivity. Checks user's input and moves to GameActivity.

--- app/src/main/java/cs245/concentration/StartActivity.java
+++ app/src/main/java/cs245/concentration/StartActivity.java
@@ -1,24 +1,53 @@
 package cs245.concentration;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by MingKie on 2/22/2017.
  */
 
 public class StartActivity extends AppCompatActivity{
+
     Button submit;
+    EditText userInput;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        userInput = (EditText)findViewById(R.id.input);
+        submit = (Button)findViewById(R.id.submitBtn);
 
 
-        Button submitBtn = (Button)findViewById(R.id.submitBtn);
+        submit.setOnClickListener(new View.OnClickListener() {
+
+
+                @Override
+                public void onClick(View view) {
+                    String tempInput = userInput.getText().toString();
+                    int input = Integer.parseInt(tempInput);
+                    if (input > 20) {
+                        userInput.requestFocus();
+                        userInput.setError("CANNOT BE GREATER 20");
+                    } else if (input < 2) {
+                        userInput.requestFocus();
+                        userInput.setError("CANNOT BE LESS THEN 2");
+                    } else if (input % 2 != 0) {
+                        userInput.requestFocus();
+                        userInput.setError("CANNOT BE AN ODD NUMBER");
+                    } else {
+                        Intent i = new Intent(StartActivity.this, GameActivity.class);
+                        startActivity(i);
+                    }
+                }
+        });
 
 
     }
