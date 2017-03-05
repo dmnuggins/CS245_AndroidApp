7159f5f982194c62d7df2d40c49b750a5ffa115b
Dylan Nguyen <dylanminhnguyen@gmail.com>
2017-03-05 03:35:45 -0800

    Revert "Added music"
    This reverts commit 36040f9fde38d5dfbfff15fa1eba5d250589f6c9.

--- app/src/main/java/cs245/concentration/GameActivity.java
+++ app/src/main/java/cs245/concentration/GameActivity.java
@@ -1,13 +1,8 @@
 package cs245.concentration;
 
-import android.app.ActivityManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,15 +11,12 @@ import android.widget.GridView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import cs245.concentration.Game.CardAdapter;
 
 public class GameActivity extends AppCompatActivity {
 
-    MediaPlayer player;
     GridView gridView;
-//    Button mBtn = (Button) findViewById(R.id.musicButton);
     int input = 0;
 
     private final String[] answers = new String[]{
@@ -48,25 +40,6 @@ public class GameActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), gridView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
             }
         });
-
-
-        player = MediaPlayer.create(this, R.raw.metal_gear_solid_alert_theme);
-        player.setLooping(true);
-        player.setVolume(100,100);
-        player.start();
-
-        Button musicOnOff = (Button) findViewById(R.id.musicButton);
-        musicOnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(player.isPlaying()) {
-                    player.pause();
-                } else {
-                    player.start();
-                }
-            }
-        });
-
     }
 
     public void newGame(View view) {
@@ -74,18 +47,14 @@ public class GameActivity extends AppCompatActivity {
         // reset everything etc
         // actually already resets everything so that's nice
         startActivity(intent);
-        finish();
-        player.stop();
     }
 
     public void tryAgain(View vew) {
         // will do later
-        player.pause();
     }
 
     public void endGame(View view) {
         // will do later
-        player.start();
     }
 
     protected ArrayList<String> cardList(int input) {
@@ -100,20 +69,5 @@ public class GameActivity extends AppCompatActivity {
         return list;
     }
 
-    @Override
-    protected void onPause() {
-        if (this.isFinishing()){ //basically BACK was pressed from this activity
-            player.stop();
-        }
-        Context context = getApplicationContext();
-        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
-        if (!taskInfo.isEmpty()) {
-            ComponentName topActivity = taskInfo.get(0).topActivity;
-            if (!topActivity.getPackageName().equals(context.getPackageName())) {
-                player.stop();
-            }
-        }
-        super.onPause();
-    }
+
 }
