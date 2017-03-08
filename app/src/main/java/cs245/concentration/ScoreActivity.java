9614691d13efb0f3551b7485c0168455178cbd77
Dylan Nguyen <dylanminhnguyen@gmail.com>
2017-03-08 01:18:09 -0800

    Game + highscores
    - game functions
    - name dialog
    - end score screen

--- app/src/main/java/cs245/concentration/ScoreActivity.java
+++ app/src/main/java/cs245/concentration/ScoreActivity.java
@@ -1,25 +1,44 @@
 package cs245.concentration;
 
-import android.content.Context;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import cs245.concentration.Game.CustomPagerAdapter;
+/**
+ * Created by MingKie on 3/7/2017.
+ */
 
-public class ScoreActivity extends AppCompatActivity{
+public class ScoreActivity extends AppCompatActivity {
 
-    @Override
+    //private int difficulty;
+    //private int score;
+    //private String name;
+    private TextView nameTxt;
+    private TextView scoreTxt;
+    private TextView diffcultyTxt;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+        Intent intent = getIntent();
+        int difficulty = intent.getIntExtra("difficulty", 0);
+        String name = intent.getStringExtra("name");
+        int score = intent.getIntExtra("score", 0);
+        nameTxt = (TextView) findViewById(R.id.name);
+        scoreTxt = (TextView) findViewById(R.id.score);
+        diffcultyTxt = (TextView) findViewById(R.id.difficulty);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new CustomPagerAdapter(this));
+        //Intent intent = getIntent();
+        //difficulty = intent.getIntExtra("difficulty", 0);
+        //name = intent.getStringExtra("name");
+        //score = intent.getIntExtra("score", 0);
+
+        scoreTxt.setText(Integer.toString(score));
+        nameTxt.setText(name);
+        diffcultyTxt.setText(Integer.toString(difficulty));
+
 
 
     }
-
 }
