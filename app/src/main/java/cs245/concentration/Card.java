8026468546ce1ef96337ce9c0d47f64491436204
Eugene Lee <eugenelee@cpp.edu>
2017-03-08 13:20:01 -0800

    replaced button with imagebutton for image scaling

--- app/src/main/java/cs245/concentration/Card.java
+++ app/src/main/java/cs245/concentration/Card.java
@@ -1,18 +1,19 @@
 package cs245.concentration;
 
 import android.widget.Button;
+import android.widget.ImageButton;
 
 //To represent a card object.
 public class Card{
 
     public int x; // column number
     public int y; // row number
-    public Button button; // its button
+    public ImageButton button; // its button
     public boolean flipped = false; // to indicate if it's flipped
 
     //method: Card()
     //purpose:Tto create a new card object.
-    public Card(Button button, int x,int y) {
+    public Card(ImageButton button, int x, int y) {
         this.x = x;
         this.y=y;
         this.button=button;
